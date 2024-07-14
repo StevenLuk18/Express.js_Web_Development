4902ad6c16788b21b27a26c76567bc4803671f06
Steven Luk <mrchun96@gmail.com>
2024-07-15 02:29:54 +0800

    Update routes folder

--- routes/myprofile.js
+++ routes/myprofile.js
@@ -5,6 +5,10 @@ var path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/";
 const client = new MongoClient(url);
+const multer = require('multer');
+const upload = multer({dest:'tmp/', limits: {fileSize:1024*1024 /* in bytes */}}); //dest 可自己define
+const fs = require('fs');
+
 
 router.use((req, res, next) => {
     res.locals.currUser = req.session.authUser;
@@ -14,62 +18,123 @@ router.use((req, res, next) => {
 router.get('/', (req, res, next) => {
     user_name = req.session.authUser //set variable as the session
 
-    if (!user_name) res.sendFile(path.join(__dirname,'..','private_web','myprofile.html'));
+    if (!user_name) res.redirect('/');
     else res.render(path.join('','mb','myprofile_mb'), {u_name : user_name[2]}); //Must add {u_name : req.session.user[2]}
 
-}).post('/infoUpdate', async (req, res, next) => {
+}).post('/infoUpdate', upload.single('image'),async (req, res, next) => {
 
-    const { name, username, gender, pw2, email, favoriteCities, preferredTransport } = req.body;
-    
     try {
+      // handle image upload
+    let newFileName;
+    if (req.file) {
+      const fileExtension = path.extname(req.file.originalname);
+      newFileName = `test${fileExtension}`;
+      fs.renameSync(req.file.path, `public/images/member/${newFileName}`);
+    }
+
       await client.connect();
       const db =client.db('travel').collection('member');
-      let data = await db.findOne({Name: req.session.authUser[2]});
-      if (data) {
-      data.mpusername = req.body.username;
-      data.mpname = req.body.name;
-      data.mpgender = req.body.gender;
-      data.mppswd = req.body.pw2;
-      data.mpemail =req.body.email;
-      
-      // handle favCities
-      req.body.favoriteCities = favCities
+      let data = await db.findOne({mpemail: req.body.email});
 
-      Object.keys(favCities).foreach( el => {
-        if ( el === req.body) {
-          Object.keys(el).filter(key => {
-            if(key.startsWith('Taiwan')) { data.mptaiwan = 'T';}
-            if(key.startsWith('China')) { data.mpchina = 'T';}
-            if(key.startsWith('Japan')) { data.mpjapan = 'T';}
-            if(key.startsWith('Korean')) { data.mpkorean = 'T';}
-            if(key.startsWith('Europe')) { data.mpeurope = 'T';}
-            if(key.startsWith('USA')) { data.mpusa = 'T';}
-            if(key.startsWith('England')) { data.mpengland = 'T';}
-            if(key.startsWith('Canada')) { data.mpcanada = 'T';}
+        if (data) {
+          data.mpusername = req.body.nickname;
+          data.mpname = req.body.acname;
+          data.mpgender = req.body.gender;
+          data.mppswd = req.body.pw2;
+          data.mpemail =req.body.email;
+          data.mpcntyothdesc = req.body.othercountry;
+          data.mptranothdesc = req.body.othertran;
+
+          console.log(req.body)
+
+          const favoriteCitiesArray = JSON.parse(req.body.favoriteCities);
+          const preferredTransportArray = JSON.parse(req.body.preferredTransport);
+          
+          console.log(favoriteCitiesArray);
+          console.log(preferredTransportArray);
+          
+          data.mpchina = 'F',
+          data.mpjapan =  'F',
+          data.mptaiwan = 'F',
+          data.mpkorean = 'F',
+          data.mpeurope = 'F',
+          data.mpusa = 'F',
+          data.mpengland = 'F',
+          data.mpcanada = 'F',
+          data.mpcntyother = 'F',
+          data.mpairplan = 'F',
+          data.mpcruise = 'F',
+          data.mptrain = 'F',
+          data.mprail = 'F',
+          data.mptranother = 'F'
+
+          await favoriteCitiesArray.forEach(el => {
+            switch(el) {
+              case 'Taiwan':
+                data.mptaiwan = 'T';
+                break;
+              case 'China':
+                data.mpchina = 'T';
+                break;
+              case 'Japan':
+                data.mpjapan = 'T';
+                break;
+              case 'Korea':
+                data.mpkorean = 'T';
+                break;
+              case 'Europe':
+                data.mpeurope = 'T';
+                break;
+              case 'USA':
+                data.mpusa = 'T';
+                break;
+              case 'England':
+                data.mpengland = 'T';
+                break;
+              case 'Canada':
+                data.mpcanada = 'T';
+                break;
+              case 'Others':
+                data.mpcntyother = 'T';
+                break;
+            }
+          });
+
+          await preferredTransportArray.forEach(el => {
+            switch(el) {
+              case 'Airplan':
+                data.mpairplan = 'T';
+                break;
+              case 'Cruise':
+                data.mpcruise = 'T';
+                break;
+              case 'Train':
+                data.mptrain = 'T';
+                break;
+              case 'High-speed rail':
+                data.mprail = 'T';
+                break;
+              case 'Others':
+                data.mptranother = 'T';
+                break;
+              }
+          }) 
+
+        await db.replaceOne({_id:data._id}, data);
+
+          // send to front-end for updating new info
+          res.json({
+            password: req.body.pw2,
+            email: req.body.email,
+            nickname: req.body.nickname,
+            gender: req.body.gender,
+
           })
-        }
-      });
-
-      req.body.preferredTransport = favTransport
-
-      Object.keys(favTransport).foreach( el => {
-        if ( el === req.body) {
-          Object.keys(el).filter(key => {
-            if(key.startsWith('Airplan')) { data.mpairplan = 'T';}
-            if(key.startsWith('Cruise')) { data.mpcruise = 'T';}
-            if(key.startsWith('Train')) { data.mptrain = 'T';}
-            if(key.startsWith('highspeedrail')) { data.mprail = 'T';}
-          })
-        }
-      });      
-
-      await db.replaceOne({_id:data._id}, data);
-      }
-      res.send(`Here are records changed!!!`)
+        } 
 
   } finally {
       await client.close()
   }
-});
+})
 
 module.exports = router;
